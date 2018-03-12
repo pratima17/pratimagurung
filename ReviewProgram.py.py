681a7e50a61ca82bd92a29168f8a18b8acaa06f0
Pratima Gurung <pratima.gurung@stu.jefferson.kyschools.us>
2018-03-12 08:26:18 -0400

    first commit

---
+++ ReviewProgram.py.py
@@ -0,0 +1,15 @@
+ title =input ("what is the title?:)
+ media_type =input ("what is the media type?:)
+ game =input ("what is the game")
+ genre = input ("what is genre?:)
+ description = input ("descrip of media_type")
+ rating = input ("what is rating?")
+ float =(" racting") var = input("what is your input?")
+ years = input ("which year it's realease ? ")
+
+new_list =[media_type,game,genre,description,rating,tittle,year,float,)
+print ("tittle ,genre,media, type rating, description)
+                
+
+                
+ 
